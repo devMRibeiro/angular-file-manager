9a1918fa5e3fb330a6ef9845fe5fb5093b78a54f
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-15 23:09:20 -0300

    Create Component for File Upload UI

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,6 +10,38 @@ import { Component } from '@angular/core';
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.css'
 })
-export class FileUploadComponent {
+export class FileUploadComponent implements OnInit {
+  currentFile?: File;
+  message = '';
+  fileInfos?: Observable<any>;
+  
+  constructor(private uploadService: FileUploadService) { }
 
+  upload(): void {
+    if (this.currentFile) {
+      this.uploadService.upload(this.currentFile).subscribe({
+        next: (event: any) => {
+          if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            this.fileInfos = this.uploadService.getFiles();
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+
+          if (err.error && err.error.message)
+            this.message = err.error.message;
+          else
+            this.message = 'Could not upload the file!';
+        },
+        complete: () => {
+          this.currentFile = undefined;
+        },
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.fileInfos = this.uploadService.getFiles();
+  }
 }
