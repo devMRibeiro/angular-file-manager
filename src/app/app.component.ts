378438698bfde44f33918714db8afd7cec5abe4e
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-16 02:14:27 -0300

    Add imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FileUploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
